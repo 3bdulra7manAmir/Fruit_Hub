6cf2d5abf98aeab4e16a13cc594cede05027c9de
Shadow <ethanhunt4d@gmail.com>
2025-08-06 10:46:29 +0300

    mark all as read done

---
+++ lib/features/05_notifications/presentation/controllers/mark_as_read_controller.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mark_as_read_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$markAsReadHash() => r'4a0f64eaf8fb85c8b27e83af6899dd2676b02165';
+
+/// See also [MarkAsRead].
+@ProviderFor(MarkAsRead)
+final markAsReadProvider =
+    AutoDisposeNotifierProvider<MarkAsRead, bool>.internal(
+  MarkAsRead.new,
+  name: r'markAsReadProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$markAsReadHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MarkAsRead = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/features/05_notifications/presentation/widget/title_body.dart
+++ lib/features/05_notifications/presentation/widget/title_body.dart
@@ -1,13 +1,14 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/constants/app_sizes.dart';
+import '../controllers/mark_as_read_controller.dart';
 import 'title_all_read.dart';
 import 'title_count.dart';
 import 'title_header.dart';
+
 
 class NotificationsTitleWidget extends StatelessWidget
 {
@@ -31,14 +32,10 @@ class NotificationsTitleWidget extends StatelessWidget
         Consumer(
           builder: (_, ref, _)
           {
-            //final markAsRead = ref.watch(provider);
             return GestureDetector(
-              onTap: ()
-              {
-                {
-                  log('Remove All Green Color Or Remove From List did not decide Know Yet...',);
-                  
-                }
+              onTap: () {
+                log('Remove All Green Color Or Remove From List did not decide Know Yet...',);
+                ref.read(markAsReadProvider.notifier).toggle();
               },
               child: const MarkAllAsReadWidget(),
             );
@@ -48,3 +45,4 @@ class NotificationsTitleWidget extends StatelessWidget
     );
   }
 }
+
