12b45dd9b0664a64b5c81e989a6fc3826b4857aa
Shadow <ethanhunt4d@gmail.com>
2025-08-06 16:38:11 +0300

    adding firebase to the project

---
+++ lib/core/services/firebase/firebase_init.dart
@@ -0,0 +1,23 @@
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
+import '../../../firebase_options.dart';
+
+
+class FirebaseInitial
+{
+
+  Future<void> firebaseInit() async
+  {
+    try
+    {
+      await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+      log('Firebase Init Done... no issues');
+    }
+    on Exception catch (e, stack)
+    {
+      log('error Within Firebase Init... error is: $e ${'\n\n'} error Stack: $stack');
+    }
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'core/services/firebase/firebase_init.dart';
 import 'main_app.dart';
 
 void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,]);
+  await FirebaseInitial().firebaseInit();
   
   runApp(const ProviderScope(child: ECommerceApp()));
 }
