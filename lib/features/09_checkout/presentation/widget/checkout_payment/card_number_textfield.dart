c3ca7c7d4a25a57a2dde194e4c81d1502e183adb
Shadow <ethanhunt4d@gmail.com>
2025-08-13 22:41:49 +0300

    ci test

--- lib/features/09_checkout/presentation/widget/checkout_payment/card_number_textfield.dart
+++ lib/features/09_checkout/presentation/widget/checkout_payment/card_number_textfield.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 import '../../../../../config/i18n/generated/l10n.dart';
 import '../../../../../core/services/validation/validation.dart';
-import '../../../../../core/utils/ui/input_formatters.dart';
 import '../../../../../core/widgets/textform_field.dart';
 
 class CardNumberWidget extends StatelessWidget
