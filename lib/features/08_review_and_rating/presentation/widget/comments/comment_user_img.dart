81affd3b0495dface0e58f6a3c65c78519895a37
Shadow <ethanhunt4d@gmail.com>
2025-08-13 20:41:51 +0300

    ci test

--- lib/features/08_review_and_rating/presentation/widget/comments/comment_user_img.dart
+++ lib/features/08_review_and_rating/presentation/widget/comments/comment_user_img.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '/../core/utils/ui/random_color.dart';
 import '../../../../../config/theme/color_manager/colors.dart';
 import '../../../../../core/constants/app_borders.dart';
 import '../../../../../core/constants/app_images.dart';
 import '../../../../../core/constants/app_styles.dart';
-import '../../../../../core/utils/ui/random_color.dart';
 
 class RatingUserImgBody extends StatelessWidget
 {
